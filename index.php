ebb47f59b86323f5182f5ae974e884b43e5b3fb7
Hwang9170 <jkimg9170@naver.com>
2025-03-14 20:43:55 +0900

    PHP START

---
+++ index.php
@@ -0,0 +1,11 @@
+<HTML><HEAD>
+<TITLE>PHP 테스트</TITLE>
+</HEAD>
+<BODY>
+<?php
+$week[] = "월"; $week[] = "화"; $week[] = "수";
+//비어있는 대괄호는 배열을 상징함.
+print $week[0]; print "<BR>";
+print $week[1]; print "<BR>"; print $week[2];
+?>
+</BODY></HTML>
